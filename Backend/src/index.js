51c5b132dcfc713c04df43d0b3eac66b5d176a52
alokkumar-dev <alokshakya2601@gmail.com>
2022-06-06 23:44:50 +0530

    modify file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,13 +4,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 const connect = require("./configs/db");
 
-app.listen(port, async()=>{
-    try{
-        await connect();
-        console.log(`listening on port ${port}`);
-
-    }
-    catch(error){
-        console.log(error.message);
-    }
-})
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log(`listening on port ${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
