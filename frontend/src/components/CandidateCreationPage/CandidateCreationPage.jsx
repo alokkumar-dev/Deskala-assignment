a58fbcb065b7e907183f8fc44a997e353a364b73
alokkumar-dev <alokshakya2601@gmail.com>
2022-06-08 00:24:54 +0530

    modify files

--- frontend/src/components/CandidateCreationPage/CandidateCreationPage.jsx
+++ frontend/src/components/CandidateCreationPage/CandidateCreationPage.jsx
@@ -1,47 +1,84 @@
-import "./CandidateCreatePage.css"
+import { useState } from "react";
+import "./CandidateCreatePage.css";
 export const CandidateCreationPage = () => {
+  const [formData, setFormData] = useState({});
+  const [data, setData] = useState([]);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData([formData]);
+  };
+  console.log(data);
   return (
     <>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div className="leftSide">
           <label htmlFor="name">Name</label>
           <br />
-          <input type="text" id="name" placeholder="enter your name" />
+          <input
+            type="text"
+            id="name"
+            onChange={handleChange}
+            placeholder="enter your name"
+          />
           <br /> <br />
-          <label htmlFor="DOB">Date of Birth</label><br />
-          <input type="date" id="DOB" placeholder="enter your Date of Birth" />
-          <br /><br />
+          <label htmlFor="DOB">Date of Birth</label>
+          <br />
+          <input
+            type="date"
+            id="DOB"
+            onChange={handleChange}
+            placeholder="enter your Date of Birth"
+          />
+          <br />
+          <br />
           <label htmlFor="age">Age</label>
           <br />
-          <input type="number" id="age" placeholder="enter your age" />
+          <input
+            type="number"
+            id="age"
+            onChange={handleChange}
+            placeholder="enter your age"
+          />
           <br />
         </div>
         <div className="rightSide">
           <label htmlFor="name">Address</label>
           <br />
-          <input type="text" id="address" placeholder="enter your address" />
-          <br /><br />
+          <input
+            type="text"
+            id="address"
+            onChange={handleChange}
+            placeholder="enter your address"
+          />
+          <br />
+          <br />
 
           <label htmlFor="state">State</label>
           <br />
-          <select name="select" id="">
+          <select name="select" id="select" onChange={handleChange}>
             <option value="">Select your state</option>
             <option value="sortlist">Sortlist</option>
             <option value="rejected">Rejected</option>
           </select>
-          <br /><br />
+          <br />
+          <br />
           <label htmlFor="pinCode">Pin Code</label>
           <br />
           <input
             type="number"
             id="pincode"
+            onChange={handleChange}
             placeholder="enter your 6-digit pin code"
           />
         </div>
         <div className="formButtons">
-
-        <input type="button" id="cancelBtn" value={"Cancel"} />
-        <input type="submit" id="createBtn" value={"Create"} />
+          <input type="button" id="cancelBtn" value={"Cancel"} />
+          <input type="submit" id="createBtn" value={"Create"} />
         </div>
       </form>
     </>
